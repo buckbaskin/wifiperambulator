af10b6a0456e0292265a34d85abf51bfc5c055c0
Buck Baskin <mobile.wbaskin@gmail.com>
2016-10-08 05:33:37 +0000

    Hoping to push this

--- test.py
+++ test.py
@@ -2,14 +2,23 @@ import subprocess
 import sys
 import io
 
-print('parse output, display it again in a nice sorted way')
+import datetime
+
 command = "sudo iwlist wlan0 scanning | egrep 'Address|ESSID|Quality'"
 counter = 0
-child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-for line in child.stdout:
-    if line == '' and child.poll() != None:
-        break
-    if line != '':
+
+# from collections import defaultdict
+time = datetime.datetime.now()
+data = {}
+
+cycles = 2
+for i in range(0, cycles):
+    child = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    for line in child.stdout:
+        if line == '' and child.poll() != None:
+            break
+        if line == '':
+            continue
         decoded = line.decode().strip()
         # sys.stdout.write(decoded+'\n')
         if 'Address' in decoded:
@@ -22,9 +31,36 @@ for line in child.stdout:
              dBm_int = int(dBm_str)
              # print('dbm %d' % (dBm_int,))
         if 'ESSID' in decoded:
-             essid = decoded.split('"')[1]
-             print('ssid decode: %s' % (decoded,))
+             ssid = decoded.split('"')[1]
+             # print('ssid decode: %s' % (decoded,))
              # print('ssid %s' % (essid,))
-             print('ssid|addr|dBm - %s|%s|%d' % (essid, mac_address, dBm_int,))
+             # print('ssid|addr|dBm - %s|%s|%d' % (ssid, mac_address, dBm_int,))
+             if ssid and mac_address and dBm_int <= 0:
+                 if mac_address not in data:
+                      data[mac_address] = []
+                 data[mac_address].append((ssid, dBm_int,))
         sys.stdout.flush()
+
+print('data collected at %s' % (time,))
+
+for key in data:
+    list_of_results = data[key]
+    if len(list_of_results) <= 1:
+        continue
+    base_ssid = list_of_results[0][0]
+    for tup_ in list_of_results:
+        if tup_[0] != base_ssid:
+            print('ssid fail')
+            continue
+
+    sum_ = 0
+    max_ = -100
+    min_ = 0
+    for tup_ in list_of_results:
+        sum_ += tup_[1]
+        max_ = max(max_, tup_[1])
+        min_ = min(min_, tup_[1])
+
+    print('%s -> %s > %s > %s' % (base_ssid, max_, float(sum_)/len(list_of_results), min_,))
+sys.stdout.flush()
 
